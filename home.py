4ea7e267cba23d39d87814868d5780ecb379eb7c
rahulsingh <rahulsingh@eiu.com>
2021-01-05 15:10:02 +0530

    added port

--- home.py
+++ home.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return jsonify ({'about':'Hello World!'})
+    return jsonify({'about': 'Hello World!'})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
