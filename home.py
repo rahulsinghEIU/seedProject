105aef5225c78cc12e2a1e197125ec92543926c8
rahulsingh <rahulsingh@eiu.com>
2021-01-12 15:00:01 +0530

    updated port

--- home.py
+++ home.py
@@ -15,5 +15,5 @@ class Hello(Resource):
 api.add_resource(Hello, "/<string:name>")
 
 if __name__ == '__main__':
-     app.run(debug=True)
-    #app.run(debug=True, host='0.0.0.0', port=80)
+    #app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
