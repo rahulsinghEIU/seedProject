b7397deab7b806a146e2408a45dd0991d4d58695
rahulsingh <rahulsingh@eiu.com>
2021-01-12 14:17:54 +0530

    added requirements

--- home.py
+++ home.py
@@ -1,12 +1,19 @@
 from flask import Flask, jsonify
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+names = {"guy": "dev", "rahul": "dev2"}
 
 
-@app.route("/")
-def hello():
-    return jsonify({'about': 'Hello World!'})
+class Hello(Resource):
+    def get(self, name):
+        return names[name]
 
+
+api.add_resource(Hello, "/<string:name>")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+     app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0', port=80)
