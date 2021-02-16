2b856a8c9b1af8ab0b84d7c77dbacd0d62b8990e
Rahul Singh <RahulSingh@eiu.com>
2021-02-16 15:01:57 +0530

    updated deploy script & path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 ips=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=seed-test-asg" --query 'Reservations[].Instances[].PrivateIpAddress' --output text);
 for ip in $ips;
-    do ssh -i ~/.ssh/deploy.pem ec2-user@$ip "sudo bash -c 'cd seedProject; git pull'";
+    do ssh -i deploy.pem ec2-user@$ip "sudo bash -c 'cd seedProject; git pull'";
 done
