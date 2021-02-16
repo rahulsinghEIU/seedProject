20b0ba5569ed4a9b2e7a4d5faee70a6e9c5d5285
Rahul Singh <RahulSingh@eiu.com>
2021-02-16 14:53:14 +0530

    updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-instanceID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=seed-test-asg" --query Reservations[*].Instances[*].[InstanceId] --output text)
 
-aws ec2 terminate-instances --instance-ids $instanceID
+ips=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=seed-test-asg" --query 'Reservations[].Instances[].PrivateIpAddress' --output text);
+for ip in $ips;
+    do ssh -i ~/.ssh/deploy.pem ec2-user@$ip "sudo bash -c 'cd seedProject; git pull'";
+done
